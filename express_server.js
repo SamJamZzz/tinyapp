285afe8f1576b4023d5356689e27cbd906c89e78
Sam <samjulian6@gmail.com>
2022-10-10 20:27:09 -0700

    Remove nested and redundant routes. Add comments for improved readability.

--- express_server.js
+++ express_server.js
@@ -26,6 +26,7 @@ const users = {};
 
 app.use(express.urlencoded({ extended: true }));
 
+// Homepage route
 app.get("/", (req, res) => {
   if (!req.session.user_id) {
     return res.redirect('/login');
@@ -33,11 +34,36 @@ app.get("/", (req, res) => {
   res.redirect('/urls');
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/urls/:id/delete", (req, res) => {
+
+
+// Rendering urls routes
+// Shows list of URLs
+app.get("/urls", (req, res) => {
+  let validURLs = urlsForUser(req.session.user_id, urlDatabase);
+  if (req.session.user_id) {
+    const templateVars = { urls: validURLs, user: users[req.session.user_id] };
+    return res.render("urls_index", templateVars);
+  }
+  res.send("<html><body>You must first sign in.</body></html>\n");
+});
+
+// Shows page for creation of new short URL
+app.get("/urls/new", (req, res) => {
+  if (req.session.user_id) {
+    const templateVars = {
+      user: users[req.session.user_id]
+    };
+    return res.render("urls_new", templateVars);
+  }
+  res.redirect('/login');
+});
+
+// Shows page for given short URL
+app.get("/urls/:id", (req, res) => {
   if (!req.session.user_id) {
     return res.send("<html><body>You must be logged in to access short URLs</body></html>\n");
   }
@@ -48,10 +74,80 @@ app.get("/urls/:id/delete", (req, res) => {
     return res.send("<html><body>You do not have access to this short URL</body></html>\n");
   }
 
-  delete urlDatabase[req.params.id];
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id].longURL, user: users[req.session.user_id] };
+  res.render("urls_show", templateVars);
+});
+
+
+
+// Rendering auth routes
+// Shows page for registering new user
+app.get("/register", (req, res) => {
+  if (!req.session.user_id) {
+    const templateVars = { user: users[req.session.user_id] };
+    return res.render("register", templateVars);
+  }
   res.redirect('/urls');
 });
 
+// Shows page for an existing user to login
+app.get("/login", (req, res) => {
+  if (!req.session.user_id) {
+    const templateVars = { user: users[req.session.user_id] };
+    return res.render("login", templateVars);
+  }
+  res.redirect('/urls');
+});
+
+
+
+// urls CRUD REST API
+// Creates new short URL
+app.post("/urls", (req, res) => {
+  if (req.session.user_id) {
+    let id = generateRandomString();
+    if (!urlDatabase[id]) {
+      urlDatabase[id] = {};
+    }
+    urlDatabase[id].longURL = req.body.longURL;
+    urlDatabase[id].userID = req.session.user_id;
+    return res.redirect(`/urls/${id}`);
+  }
+  res.send("<html><body>You cannot shorten URLs without being signed in.</body></html>\n");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+// Visits the long URL associated with the given short URL
+app.get("/u/:id", (req, res) => {
+  if (!searchForURLId(req.params.id, urlDatabase)) {
+    return res.send("<html><body>Invalid short URL</body></html>\n");
+  }
+
+  if (req.session.user_id !== urlDatabase[req.params.id].userID) {
+    return res.send("<html><body>You do not have access to this short URL</body></html>\n");
+  }
+
+  const longURL = urlDatabase[req.params.id].longURL;
+  res.redirect(longURL);
+});
+
+// Updates the long URL for a given short URL
+app.post("/urls/:id", (req, res) => {
+  if (!req.session.user_id) {
+    return res.send("<html><body>You must be logged in to access short URLs</body></html>\n");
+  }
+  if (!searchForURLId(req.params.id, urlDatabase)) {
+    return res.send("<html><body>Short URL does not exist</body></html>\n");
+  }
+
+  urlDatabase[req.params.id].longURL = req.body.updatedLongURL;
+  res.redirect('/urls');
+});
+
+// Deletes the long and short URLs for the user they are associated with
 app.post("/urls/:id/delete", (req, res) => {
   if (!req.session.user_id) {
     return res.send("<html><body>You must be logged in to access short URLs</body></html>\n");
@@ -67,112 +163,8 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect('/urls');
 });
 
-app.get("/login", (req, res) => {
-  if (!req.session.user_id) {
-    const templateVars = { user: users[req.session.user_id] };
-    return res.render("login", templateVars);
-  }
-  res.redirect('/urls');
-});
-
-app.post("/login", (req, res) => {
-  let user = searchUserByEmail(req.body.email, users);
-  if (!user) {
-    return res.status(403).send('Invalid credentials');
-  }
-
-  if (!bcrypt.compareSync(req.body.password, user.password)) {
-    return res.status(403).send('Invalid credentials');
-  }
-  req.session.user_id = user.id;
-  res.redirect('/urls');
-});
-
-app.post("/logout", (req, res) => {
-  req.session = null;
-  res.redirect('/urls');
-});
-
-app.get("/urls", (req, res) => {
-  let validURLs = urlsForUser(req.session.user_id, urlDatabase);
-  if (req.session.user_id) {
-    const templateVars = { urls: validURLs, user: users[req.session.user_id] };
-    return res.render("urls_index", templateVars);
-  }
-  res.send("<html><body>You must first sign in.</body></html>\n");
-});
-
-app.post("/urls", (req, res) => {
-  if (req.session.user_id) {
-    let id = generateRandomString();
-    if (!urlDatabase[id]) {
-      urlDatabase[id] = {};
-    }
-    urlDatabase[id].longURL = req.body.longURL;
-    urlDatabase[id].userID = req.session.user_id;
-    return res.redirect(`/urls/${id}`);
-  }
-  res.send("<html><body>You cannot shorten URLs without being signed in.</body></html>\n");
-});
-
-app.get("/u/:id", (req, res) => {
-  if (!searchForURLId(req.params.id, urlDatabase)) {
-    return res.send("<html><body>Invalid short URL</body></html>\n");
-  }
-
-  if (req.session.user_id !== urlDatabase[req.params.id].userID) {
-    return res.send("<html><body>You do not have access to this short URL</body></html>\n");
-  }
-
-  const longURL = urlDatabase[req.params.id].longURL;
-  res.redirect(longURL);
-});
-
-app.get("/urls/new", (req, res) => {
-  if (req.session.user_id) {
-    const templateVars = {
-      user: users[req.session.user_id]
-    };
-    return res.render("urls_new", templateVars);
-  }
-  res.redirect('/login');
-});
-
-app.get("/urls/:id", (req, res) => {
-  if (!req.session.user_id) {
-    return res.send("<html><body>You must be logged in to access short URLs</body></html>\n");
-  }
-  if (!searchForURLId(req.params.id, urlDatabase)) {
-    return res.send("<html><body>Short URL does not exist</body></html>\n");
-  }
-  if (req.session.user_id !== urlDatabase[req.params.id].userID) {
-    return res.send("<html><body>You do not have access to this short URL</body></html>\n");
-  }
-
-  app.post("/urls/:id", (req, res) => {
-    if (!req.session.user_id) {
-      return res.send("<html><body>You must be logged in to access short URLs</body></html>\n");
-    }
-    if (!searchForURLId(req.params.id, urlDatabase)) {
-      return res.send("<html><body>Short URL does not exist</body></html>\n");
-    }
-  
-    urlDatabase[req.params.id].longURL = req.body.updatedLongURL;
-    res.redirect('/urls');
-  });
-
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id].longURL, user: users[req.session.user_id] };
-  res.render("urls_show", templateVars);
-});
-
-app.get("/register", (req, res) => {
-  if (!req.session.user_id) {
-    const templateVars = { user: users[req.session.user_id] };
-    return res.render("register", templateVars);
-  }
-  res.redirect('/urls');
-});
-
+// Auth REST API
+// Registers a new user with the given email and password credentials
 app.post("/register", (req, res) => {
   if (!req.body.email || !req.body.password) {
     return res.status(400).send('You must enter an email and password');
@@ -192,10 +184,27 @@ app.post("/register", (req, res) => {
   res.redirect('/urls');
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+// Logs in an a user that has already registered before with the given email and password
+app.post("/login", (req, res) => {
+  let user = searchUserByEmail(req.body.email, users);
+  if (!user) {
+    return res.status(403).send('Invalid credentials');
+  }
+
+  if (!bcrypt.compareSync(req.body.password, user.password)) {
+    return res.status(403).send('Invalid credentials');
+  }
+  req.session.user_id = user.id;
+  res.redirect('/urls');
 });
 
+// Logs out a user that is currently logged in
+app.post("/logout", (req, res) => {
+  req.session = null;
+  res.redirect('/urls');
+});
+
+// Server listens to incoming requests
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
